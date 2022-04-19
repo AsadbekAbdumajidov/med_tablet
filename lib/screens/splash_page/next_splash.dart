b3503989cd3397c174e63378b448535287ea923b
Asadbek <asadbekabdumajidov7@gmail.com>
2022-04-19 17:01:26 +0500

    next splash page created

--- lib/screens/splash_page/next_splash.dart
+++ lib/screens/splash_page/next_splash.dart
@@ -1,12 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class NextSplash extends StatelessWidget {
-  const NextSplash({ Key? key }) : super(key: key);
+  const NextSplash({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    final PageController controller = PageController();
+    return Scaffold(
+      body: PageView(
+        controller: controller,
+        children: <Widget>[
+          Column(
+            children:  [
+              
+            ],
+          ),
+           Column(
+            children: const [
+              Center(
+                child: Text('Secossssssssssssssnd Page'),
+              ),
+            ],
+          ),
+           Column(
+            children: const [
+              Center(
+                child: Text('Secoddddddddnd Page'),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
