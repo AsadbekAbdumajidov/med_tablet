d2002b047b413bf62867b2564eff2bcfcc6ffd92
Asadbek <asadbekabdumajidov7@gmail.com>
2022-04-21 18:09:59 +0500

    verify page done

---
+++ lib/core/components/pin_put_pak_comp.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:medic/core/components/size_konfig.dart';
+import 'package:medic/core/constants/color_const.dart';
+import 'package:pinput/pinput.dart';
+
+class PinPutComp extends StatelessWidget {
+  PinPutComp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Pinput(
+      defaultPinTheme: defaultPinTheme,
+      focusedPinTheme: focusedPinTheme,
+      submittedPinTheme: submittedPinTheme,
+      validator: (s) {
+        return s == '2222' ? null : 'Pin is incorrect';
+      },
+      pinputAutovalidateMode: PinputAutovalidateMode.onSubmit,
+      showCursor: true,
+      onCompleted: (pin) => debugPrint(pin),
+    );
+  }
+   final defaultPinTheme = PinTheme(
+    width: wi(56),
+    height: he(56),
+    textStyle:  TextStyle(
+        fontSize: he(20),
+        color: const Color.fromRGBO(30, 60, 87, 1),
+        fontWeight: FontWeight.w600),
+    decoration: BoxDecoration(
+      border: Border.all(color: const Color.fromRGBO(234, 239, 243, 1)),
+      borderRadius: BorderRadius.circular(8),
+    ),
+  );
+
+  final focusedPinTheme = PinTheme(
+    width: 56,
+    height: 56,
+    textStyle: const TextStyle(
+        fontSize: 20,
+        color: Color.fromRGBO(30, 60, 87, 1),
+        fontWeight: FontWeight.w600),
+    decoration: BoxDecoration(
+      border: Border.all(color: AppColors.black),
+      borderRadius: BorderRadius.circular(8),
+    ),
+  );
+
+  final submittedPinTheme = PinTheme(
+    width: wi(56),
+    height: he(56),
+    textStyle: const TextStyle(
+        fontSize: 20,
+        color: Color.fromRGBO(30, 60, 87, 1),
+        fontWeight: FontWeight.w600),
+    decoration: BoxDecoration(
+      border: Border.all(color: AppColors.black),
+      borderRadius: BorderRadius.circular(8),
+    ),
+  );
+
+}
--- lib/screens/verify_page/verify_page.dart
+++ lib/screens/verify_page/verify_page.dart
@@ -1,110 +1,55 @@
-
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:medic/core/components/pin_put_pak_comp.dart';
+import 'package:medic/core/components/size_konfig.dart';
 import 'package:medic/core/constants/color_const.dart';
-import 'package:pinput/pinput.dart';
-
+import 'package:medic/core/constants/style_const.dart';
+import 'package:medic/core/widgets/for_login_app_bar.dart';
 
 class LoginVerifyPage extends StatelessWidget {
-  LoginVerifyPage({Key? key}) : super(key: key);
+  const LoginVerifyPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.white,
-      appBar: AppBar(
-        leading: GestureDetector(
-          onTap: () {
-            Navigator.pop(context);
-          },
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(16, 8, 0, 8),
-            child: SvgPicture.asset('assets/icons/ic_back.svg'),
-          ),
-        ),
-        title: const Text('Verification Code'),
-      ),
+      appBar: LogAppBar().getBar(context),
       body: SafeArea(
-        child: ListView(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          children: [
-            const SizedBox(height: 24),
-            const Text(
-              'Enter your code we have sent to your email'
-            ),
-            const SizedBox(height: 4),
-           const Text(
-              '+99890 0011610'
-            ),
-            const SizedBox(height: 32),
-            Pinput(
-              defaultPinTheme: defaultPinTheme,
-              focusedPinTheme: focusedPinTheme,
-              submittedPinTheme: submittedPinTheme,
-              validator: (s) {
-                return s == '2222' ? null : 'Pin is incorrect';
-              },
-              pinputAutovalidateMode: PinputAutovalidateMode.onSubmit,
-              showCursor: true,
-              onCompleted: (pin) => debugPrint(pin),
-            ),
-            const SizedBox(height: 32),
-           
-            const SizedBox(height: 24),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children:const [
-                Text(
-                  'Didn’t receive the code?'
-                ),
-                 SizedBox(width: 4),
-                Text(
-                  'Resend'
-                )
-              ],
-            ),
-          ],
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: wi(16)),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(top: he(30), bottom: he(8)),
+                child: Text("Kodni kiriting",
+                    style: StyleText.styleBlackW700Size24),
+              ),
+              Text(
+                  "Tasdiqlash kodini +998 33 954 06 90 \ntelefon raqamiga yubordik",
+                  textAlign: TextAlign.start,
+                  style: StyleText.styleBlack38W400Size16),
+              Padding(
+                padding: EdgeInsets.only(top: he(20), bottom: he(24)),
+                child: PinPutComp(),
+              ),
+              ElevatedButton(
+                  style: StyleButton.blueAccent,
+                  onPressed: () {},
+                  child: const Text("Yuborish")),
+              Padding(
+                padding: EdgeInsets.only(top: he(26)),
+                child: Text("Kod muddati 00:13 da tugaydi",
+                    textAlign: TextAlign.start,
+                    style: StyleText.styleBlack38W400Size16),
+              ),
+              TextButton(
+                  onPressed: () {},
+                  child: const Text("Yana yuboring",
+                      style: TextStyle(color: AppColors.yellow))),
+            ],
+          ),
         ),
       ),
     );
   }
-
-  final defaultPinTheme = PinTheme(
-    width: 56,
-    height: 56,
-    textStyle: const TextStyle(
-        fontSize: 20,
-        color: Color.fromRGBO(30, 60, 87, 1),
-        fontWeight: FontWeight.w600),
-    decoration: BoxDecoration(
-      border: Border.all(color: const Color.fromRGBO(234, 239, 243, 1)),
-      borderRadius: BorderRadius.circular(8),
-    ),
-  );
-
-  final focusedPinTheme = PinTheme(
-    width: 56,
-    height: 56,
-    textStyle: const TextStyle(
-        fontSize: 20,
-        color: Color.fromRGBO(30, 60, 87, 1),
-        fontWeight: FontWeight.w600),
-    decoration: BoxDecoration(
-      border: Border.all(color: AppColors.black),
-      borderRadius: BorderRadius.circular(8),
-    ),
-  );
-
-  final submittedPinTheme = PinTheme(
-    width: 56,
-    height: 56,
-    textStyle: const TextStyle(
-        fontSize: 20,
-        color: Color.fromRGBO(30, 60, 87, 1),
-        fontWeight: FontWeight.w600),
-    decoration: BoxDecoration(
-      border: Border.all(color: AppColors.black),
-      borderRadius: BorderRadius.circular(8),
-    ),
-  );
 }
