ad2eba7770194458ae728a41d0fb912962a51746
Asadbek <asadbekabdumajidov7@gmail.com>
2022-04-23 16:58:18 +0500

    Favorite page done

--- lib/screens/tabs/cart/cart.dart
+++ lib/screens/tabs/cart/cart.dart
@@ -4,6 +4,7 @@ import 'package:medic/core/components/size_konfig.dart';
 import 'package:medic/core/constants/color_const.dart';
 import 'package:medic/core/constants/style_const.dart';
 import 'package:medic/core/widgets/for_tabs_app_bar.dart';
+import 'package:medic/screens/tabs/cart/widgets/builder.dart';
 
 class Cart extends StatelessWidget {
   const Cart({Key? key}) : super(key: key);
@@ -38,123 +39,8 @@ class Cart extends StatelessWidget {
                 ),
               ],
             ),
-            SizedBox(
-              height: he(413),
-              child: ListView.builder(
-                  physics: const BouncingScrollPhysics(),
-                  padding: EdgeInsets.symmetric(vertical: he(20)),
-                  itemCount: 2,
-                  itemBuilder: (_, __) {
-                    return Column(
-                      children: [
-                        Row(children: [
-                          Container(
-                              height: he(140),
-                              width: wi(80),
-                              decoration: const BoxDecoration(
-                                  image: DecorationImage(
-                                      image: AssetImage("assets/png/dori2.png"),
-                                      fit: BoxFit.cover))),
-                          SizedBox(width: wi(5)),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                children: [
-                                  Row(
-                                    children: [
-                                      Icon(Icons.star,
-                                          color: AppColors.yellow,
-                                          size: he(16)),
-                                      Icon(Icons.star,
-                                          color: AppColors.yellow,
-                                          size: he(16)),
-                                      Icon(Icons.star,
-                                          color: AppColors.yellow,
-                                          size: he(16)),
-                                      Icon(Icons.star,
-                                          color: AppColors.yellow,
-                                          size: he(16)),
-                                      Icon(Icons.star,
-                                          color: AppColors.yellow,
-                                          size: he(16)),
-                                    ],
-                                  ),
-                                  SizedBox(width: wi(140)),
-                                  Icon(Icons.favorite_border_rounded,
-                                      color: AppColors.black38, size: he(20))
-                                ],
-                              ),
-                              SizedBox(height: he(5)),
-                              Text(
-                                "Tantum Verde uchun \nspreyitopikal qo'llash, 30 ml",
-                                style: StyleText.styleBlackW400Size12,
-                              ),
-                              Padding(
-                                padding: EdgeInsets.only(top: he(20)),
-                                child: Text(
-                                  "Sotuvda mavjud",
-                                  style: StyleText.styleBlueAccentW400Size14,
-                                ),
-                              ),
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text("285 000 so'm",
-                                      style: StyleText.styleBlackW700Size18),
-                                  SizedBox(width: wi(16)),
-                                  Row(
-                                    children: [
-                                      Row(
-                                        children: [
-                                          CircleAvatar(
-                                              radius: he(14),
-                                              backgroundColor: AppColors
-                                                  .primary100
-                                                  .withOpacity(0.4),
-                                              child: const Icon(Icons.remove,
-                                                  color: AppColors.primary100)),
-                                          Padding(
-                                            padding: EdgeInsets.symmetric(
-                                                horizontal: wi(10)),
-                                            child: Text("0",
-                                                style: StyleText
-                                                    .styleBlackW700Size18),
-                                          ),
-                                          CircleAvatar(
-                                            radius: he(14),
-                                            child: const Icon(Icons.add,
-                                                color: AppColors.white),
-                                            backgroundColor:
-                                                AppColors.primary100,
-                                          ),
-                                        ],
-                                      ),
-                                      IconButton(
-                                          onPressed: () {},
-                                          icon: Icon(
-                                              Icons.shopping_cart_outlined,
-                                              size: he(24))),
-                                    ],
-                                  ),
-                                ],
-                              )
-                            ],
-                          )
-                        ]),
-                        Padding(
-                          padding: EdgeInsets.only(
-                              left: wi(15), right: wi(15), bottom: he(20)),
-                          child: Divider(
-                              color: AppColors.black38, height: he(2.5)),
-                        ),
-                      ],
-                    );
-                  }),
-            ),
-            Column(
+             const CartBuilder(),
+             Column(
               children: [
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
--- lib/screens/tabs/favorites/favorites.dart
+++ lib/screens/tabs/favorites/favorites.dart
@@ -1,12 +1,87 @@
 import 'package:flutter/material.dart';
+import 'package:medic/core/components/size_konfig.dart';
+import 'package:medic/core/constants/color_const.dart';
+import 'package:medic/core/constants/style_const.dart';
+import 'package:medic/core/widgets/for_tabs_app_bar.dart';
 
 class Favorites extends StatelessWidget {
-  const Favorites({ Key? key }) : super(key: key);
+  const Favorites({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: HomeAppBar(
+              title: "Yoqtirilganlar", rightIcon: Icons.more_vert_rounded)
+          .getBar(context),
+      body: GridView.builder(
+        padding: EdgeInsets.only(left: wi(24), top: he(10)),
+        scrollDirection: Axis.vertical,
+        physics: const BouncingScrollPhysics(),
+        itemCount: 14,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          mainAxisExtent: he(260),
+          crossAxisCount: 2,
+        ),
+        itemBuilder: (_, __) => Stack(
+          children: [
+            Container(
+              width: wi(160),
+              margin:
+                  EdgeInsets.only(right: wi(20), top: he(10), bottom: he(10)),
+              decoration: BoxDecoration(
+                  color: AppColors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: AppColors.black38.withOpacity(0.1),
+                      spreadRadius: 2,
+                      blurRadius: 4,
+                      offset: const Offset(0, 7),
+                    ),
+                  ],
+                  borderRadius: const BorderRadius.all(Radius.circular(10))),
+              child: Padding(
+                padding: EdgeInsets.all(he(8)),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      height: he(120),
+                      decoration: const BoxDecoration(
+                          image: DecorationImage(
+                              image: AssetImage("assets/png/dori.png"))),
+                    ),
+                    Text("Nurofen bolalar uchunsuspenziya 230 ml",
+                        style: StyleText.styleBlackW400Size12),
+                    SizedBox(height: he(18)),
+                    Row(
+                      children: [
+                        Icon(Icons.star, color: AppColors.yellow, size: he(18)),
+                        Icon(Icons.star, color: AppColors.yellow, size: he(18)),
+                        Icon(Icons.star, color: AppColors.yellow, size: he(18)),
+                        Icon(Icons.star, color: AppColors.yellow, size: he(18)),
+                        Icon(Icons.star,
+                            color: AppColors.black38, size: he(18)),
+                      ],
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: he(8), bottom: he(10)),
+                      child: Text("131 000 so'm.",
+                          style: StyleText.styleActionRedW700Size16),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
+              top: he(8),
+                child: IconButton(
+                  splashRadius: 1,
+              onPressed: () {},
+              icon:  Icon(Icons.favorite, color: AppColors.actionRed,size: he(25),),
+            ))
+          ],
+        ),
+      ),
     );
   }
 }
